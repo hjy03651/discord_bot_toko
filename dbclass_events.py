4af7cbd0153d79954f254db4c7cb87f8696b7ab6
JaeyoungHan <100887215+hjy03651@users.noreply.github.com>
2024-10-20 01:49:19 +0900

    tokobot v1.2.0
    for private server.

---
+++ dbclass_events.py
@@ -0,0 +1,22 @@
+import psycopg2
+
+
+class DatabasesEvents:
+    def __init__(self):
+        self.db = psycopg2.connect(host='', dbname='', user='',
+                                   password='', port='')
+        self.cursor = self.db.cursor()
+
+    def __del__(self):
+        self.db.close()
+        self.cursor.close()
+
+    def execute(self, query, args=None):
+        if args is None:
+            args = {}
+        self.cursor.execute(query, args)
+        row = self.cursor.fetchall()
+        return row
+
+    def commit(self):
+        self.cursor.commit()
